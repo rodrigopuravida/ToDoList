b57d5c2e218848e6990bbbb6a67da1579398d746
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-01-11 18:34:18 -0800

    to do app

--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!);
+        item.text = "";
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
+    @IBOutlet weak var item: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +29,15 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true);
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        item.resignFirstResponder();
+        return true;
     }
 
 
